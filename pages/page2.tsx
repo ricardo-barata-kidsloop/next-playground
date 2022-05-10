ba594e4020120290b30f08e7f1590d58a68b4894
Ricardo Barata <ricardo.barata@kidsloop.live>
2022-05-10 10:04:15 +0100

    responsive layout on the elements api thingy

--- pages/page2.tsx
+++ pages/page2.tsx
@@ -1,15 +1,30 @@
 import { API } from '@stoplight/elements';
 import '@stoplight/elements/styles.min.css';
-import { ReactElement } from 'react';
+import useWindowSize from 'hooks/useWindowSize';
+import { ReactElement, useEffect, useState } from 'react';
 import Layout from '../components/Layout/Layout';
 
+type Layout = 'sidebar' | 'stacked';
 export default function Page2() {
+
+    const { width, height } = useWindowSize();
+
+    const [layout, setLayout] = useState<Layout>("sidebar");
+
+    useEffect(() => {
+        if (width < 600) {
+            setLayout('stacked');
+        } else {
+            setLayout('sidebar');
+        }
+    }, [width]);
+
     return (
         <API
             apiDescriptionUrl="https://swagger-ui.kidsloop.net/config/api.swagger.json"
             router="history"
             hideTryIt
-            layout="sidebar"
+            layout={layout}
             basePath="/page2/"
         />
     );
